6334d60e421937b86c20dd28b6054c48f3fc508d
Sukrutha <sbudda@govst.edu>
2026-03-13 09:11:00 -0500

    Create SQLite database at startup

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<BookstoreContext>();
-    db.Database.Migrate();
+    db.Database.EnsureCreated();
 }
 
 // Configure the HTTP request pipeline.
